3e6ae086c5ab54af0f7204f0623909ba0aa94971
Victoire Youngui <victoire.ntchuigwa@2026.ucac-icam.com>
2024-01-14 18:56:50 +0100

    beginning of the footer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {DesignedFutureComponent} from "../designed-future/designed-future.component";
 import {Section2Component} from "../section2/section2.component";
+import {Section3Component} from "../section3/section3.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
     DesignedFutureComponent,
-    Section2Component
+    Section2Component,
+    Section3Component
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
--- src/app/services/main-text-content.service.ts
+++ src/app/services/main-text-content.service.ts
@@ -23,7 +23,22 @@ export class MainTextContentService {
     title : "State of the Art Infrastructure",
     paragraph: "With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. " +
       "This ensures your site will load instantly, no matter where your readers are, keeping your site competitive."
-  }
+  };
+
+  section3TextContent: mainTextContentModel[] = [
+    {
+      title : "Free, open, simple",
+      paragraph: "Blogr is a free and open source application backed by a large community of helpful developers. " +
+        "It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party " +
+        "commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively " +
+        "easy to learn."
+    },
+    {
+      title: "Powerful tooling",
+      paragraph: "Batteries is included. We built a simple and straightforward CLI tool that makes customization and" +
+        "deployment a breeze, but capable of producing even the most complicated sites"
+    }
+  ]
 
 
 }
